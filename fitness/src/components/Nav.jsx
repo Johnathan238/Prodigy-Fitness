397d214f5c0c4d148e0456389a4f0551653ae85b
Johnathan238 <jonathanumana86@gmail.com>
2020-10-25 18:27:39 -0400

    Working on Nav--Bar

--- fitness/src/components/Nav.jsx
+++ fitness/src/components/Nav.jsx
@@ -5,12 +5,29 @@ import './Nav.css'
 const Nav = () => {
   return (
     <nav className="nav">
+      
       <Link to="/Plans">
         <p className="header--create">Plans</p>
       </Link>
       <Link to="/Schedule With Me">
         <p className="header--create">Schedule With Me</p>
       </Link>
+      
+      <div className="Mobile--Icon">
+        {/* <FaBars /> */}
+      </div>
+      
+      <div className="Nav--Menu">
+        <div className="Nav--Item">
+          
+          <div className="Nav--Links">
+
+          </div>
+
+        </div>
+      </div>
+
+
     </nav>
   )
 }
