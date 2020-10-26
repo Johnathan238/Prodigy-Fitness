d394e5af15f10e9ce41fea7069b01e3289d31230
Johnathan238 <jonathanumana86@gmail.com>
2020-10-26 00:20:14 -0400

    Added workout plans page

---
+++ fitness/src/pages/Plans.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Component } from 'react'
+
+export default class Plans extends Component {
+  render() {
+    return (
+      <div className="Home">
+
+        <h1>Are You Ready to Become A New You!</h1>
+
+        <div className="Plan--1">
+          <h2>200-Plan: 10% Discount YOU PAY: $180</h2>
+          <h3>8 SESSIONS</h3>
+          <ul>
+            <li>2 30-minute sessions per week for 4 weeks</li>
+            <li>1 on 1 coaching; Online or In-Person </li>
+            <li>Nutrition guide/tips </li>
+            <li>Accountability </li>
+            <li>24/7 access to me for checkup, support, and motivation </li>
+            <li>Progress tracking every 2 weeks: weight assessed, progress photos taken </li>
+          </ul>
+        </div>
+
+        <div className="Plan--2">
+          <h2>440-Plan: 15% Discount YOU PAY: $374</h2>
+          <h3>8 SESSIONS</h3>
+          <ul>
+            <li>2 30-minute sessions per week for 4 weeks</li>
+            <li>1 on 1 coaching; Online or In-Person </li>
+            <li>Nutrition guide/tips </li>
+            <li>Accountability </li>
+            <li>24/7 access to me for checkup, support, and motivation </li>
+            <li>Progress tracking every 2 weeks: weight assessed, progress photos taken </li>
+          </ul>
+        </div>
+
+        <div className="Plan--3">
+          <h2>400-Plan: 15% Discount YOU PAY: $512</h2>
+          <h3>16 SESSIONS</h3>
+          <ul>
+            <li>4 30-minute sessions per week for 4 weeks </li>
+            <li>1 on 1 coaching; Online or In-Person </li>
+            <li>Nutrition guide/tips </li>
+            <li>Accountability </li>
+            <li>24/7 access to me for checkup, support, and motivation </li>
+            <li>Progress tracking every 2 weeks: weight assessed, progress photos taken </li>
+          </ul>
+        </div>
+
+        <div className="Plan--4">
+          <h2>880-Plan: 25% Discount YOU PAY: $660 *MOST POPULAR*</h2>
+          <h3>16 SESSIONS</h3>
+          <ul>
+            <li> 60-minute sessions per week for 4 weeks </li>
+            <li>1 on 1 coaching; Online or In-Person </li>
+            <li>Nutrition guide/tips </li>
+            <li>Accountability </li>
+            <li>24/7 access to me for checkup, support, and motivation </li>
+            <li>Progress tracking every 2 weeks: weight assessed, progress photos taken </li>
+          </ul>
+        </div>
+
+        
+
+      </div>
+    )
+  }
+}
+
+
---
+++ fitness/src/pages/Schedule.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// // import { Link } from 'react-router-dom'
+// import { Component } from 'react'
+// import './Home.scss'
+
+// export default class Schedule extends Component {
+//   render() {
+//     return (
+//       <div className="Schedule">
+      
+        
+
+//       </div>
+//     )
+//   }
+// }
