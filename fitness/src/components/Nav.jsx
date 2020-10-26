4c40bb81645c77108ef9dc503c5915200c8e3daa
Johnathan238 <jonathanumana86@gmail.com>
2020-10-25 22:40:57 -0400

    Still going to work on Nav

--- fitness/src/components/Nav.jsx
+++ fitness/src/components/Nav.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
 import './Nav.css'
 
 const Nav = () => {
   return (
     <nav className="nav">
-      
-      <Link to="/Plans">
-        <p className="header--create">Plans</p>
-      </Link>
-      <Link to="/Schedule With Me">
-        <p className="header--create">Schedule With Me</p>
-      </Link>
-      
+
+
       <div className="Mobile--Icon">
-        {/* <FaBars /> */}
+        <FaBars />
       </div>
-      
+
       <div className="Nav--Menu">
         <div className="Nav--Item">
-          
+
           <div className="Nav--Links">
+            <Link to="/Plans">
+              <p className="header--create">About</p>
+            </Link>
+
+            <Link to="/Plans">
+              <p className="header--create">Plans</p>
+            </Link>
+
+            <Link to="/Schedule With Me">
+              <p className="header--create">Schedule With Me</p>
+            </Link>
 
           </div>
 
@@ -33,3 +39,10 @@ const Nav = () => {
 }
 
 export default Nav
+
+  // < Link to = "/Plans" >
+  //   <p className="header--create">Plans</p>
+  //           </Link >
+  // <Link to="/Schedule With Me">
+  //   <p className="header--create">Schedule With Me</p>
+  // </Link>
