8929e928aa663655de1f90c98b9a7e0d933f72ec
Johnathan238 <jonathanumana86@gmail.com>
2020-10-25 23:11:16 -0400

    Added footer

--- fitness/src/App.jsx
+++ fitness/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './components/Home'
 import Header from './components/Header'
 import Nav from './components/Nav'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 import { Route, Switch} from 'react-router-dom'
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Route exact path='/' component={Home} />
       </Switch>
 
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- fitness/src/components/Footer.jsx
+++ fitness/src/components/Footer.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-
+import './Footer.css'
 
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <body className="footer--details">
-
-        <marquee>
-          <div className="footer--details-container sei-developers">
-            <p className="footer--text">&copy; Developed by: Johnathan Umana - &copy; Developed by: Johnathan Umana - &copy; Developed by: Johnathan Umana
-            - &copy; Developed by: Johnathan Umana - &copy; Developed by: Johnathan Umana - &copy; Developed by: Johnathan Umana</p>
-          </div>
-        </marquee>
-
-        <marquee>
-          <p className="footer--siteTitle"> &copy; Vehicle-Stats-2.0 &copy; 2020 - Vehicle-Stats-2.0 &copy; 2020 -Vehicle-Stats-2.0 &copy; 2020 -
-          Vehicle-Stats-2.0 &copy; 2020 - Vehicle-Stats-2.0 &copy; 2020 - Vehicle-Stats-2.0 &copy; 2020 - Vehicle-Stats-2.0 &copy; 2020</p>
-        </marquee>
-
-      </body>
+       <a href="mailto:mbrisson23@yahoo.com" class="footer__link">mbrisson23@yahoo.com</a>
+        <ul class="social-list">
+            <li class="social-list__item">
+                <a class="social-list__link" href="https://www.linkedin.com/in/johnathan-umana-658b06170/" target="_blank">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            </li>
+            <li class="social-list__item">
+                <a class="social-list__link" href="https://github.com/Johnathan238" target="_blank">
+                    <i class="fab fa-github"></i>
+                </a>
+            </li>
+            <li class="social-list__item">
+                <a class="social-list__link" href="https://docs.google.com/forms/d/e/1FAIpQLScnMSkRHUQGU7CoQuqW4Yqdef3c_1IEHpLNsBW5KIFOmaCGXA/viewform?usp=sf_link" target="_blank">
+                    <i class="fas fa-calendar-check"></i>
+                </a>
+            </li>
+        </ul>
     </footer>
   )
 }
