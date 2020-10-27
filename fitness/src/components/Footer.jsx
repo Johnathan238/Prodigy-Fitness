a7758af5e5bc4cee35a67875bf70ce445b75372f
Johnathan238 <jonathanumana86@gmail.com>
2020-10-26 21:03:38 -0400

    Added media Icons on footer

--- fitness/src/components/Footer.jsx
+++ fitness/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa"
 import './Footer.css'
 
 
@@ -8,18 +9,18 @@ const Footer = () => {
        <a href="mailto:mbrisson23@yahoo.com" class="footer__link">mbrisson23@yahoo.com</a>
         <ul class="social-list">
             <li class="social-list__item">
-                <a class="social-list__link" href="https://www.linkedin.com/in/johnathan-umana-658b06170/" target="_blank">
-                    <i class="fab fa-linkedin"></i>
+                <a class="social-list__link" href="https://youtu.be/_caukWh354s" target="_blank">
+                    <FaYoutube />
                 </a>
             </li>
             <li class="social-list__item">
-                <a class="social-list__link" href="https://github.com/Johnathan238" target="_blank">
-                    <i class="fab fa-github"></i>
+                <a class="social-list__link" href="https://www.instagram.com/_prodigy.fitness/?hl=en" target="_blank">
+                    <FaInstagram />
                 </a>
             </li>
             <li class="social-list__item">
-                <a class="social-list__link" href="https://docs.google.com/forms/d/e/1FAIpQLScnMSkRHUQGU7CoQuqW4Yqdef3c_1IEHpLNsBW5KIFOmaCGXA/viewform?usp=sf_link" target="_blank">
-                    <i class="fas fa-calendar-check"></i>
+                <a class="social-list__link" href="https://twitter.com/_prodigyfitness" target="_blank">
+                    <FaTwitter />
                 </a>
             </li>
         </ul>
