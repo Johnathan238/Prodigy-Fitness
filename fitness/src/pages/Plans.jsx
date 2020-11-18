7ba71b261ebad5e5233bf06b961adddc0a42037e
Johnathan Umana <jonathanumana86@gmail.com.com>
2020-11-17 23:17:45 -0500

    Succesfully made Plan1 Card, Will make other cards

--- fitness/src/pages/Plans.jsx
+++ fitness/src/pages/Plans.jsx
@@ -1,35 +1,62 @@
 import React from 'react'
 import { Component } from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
 
 const Plans = () => {
   const cardInfo = [
     {
       image: '',
-      title: '200-Plan: 10% Discount YOU PAY: $180, 8 SESSIONS',
-      text:
-        '2 30-minute sessions per week for 4 weeks, 1 on 1 coaching; Online or In-Person, Nutrition guide/tips, Accountability, 24/7 access to me for checkup, support, and motivation, Progress tracking every 2 weeks: weight assessed, progress photos',
+      title1: '200-Plan: 10% Discount YOU PAY: $180',
+      title2: '8 SESSIONS',
+      list1: '2 30-minute sessions per week for 4 weeks',
+      list2: '1 on 1 coaching; Online or In-Person',
+      list3: 'Nutrition guide/tips ',
+      list4: 'Accountability',
+      list5: '24/7 access to me for checkup, support, and motivation',
+      list6: 'Progress tracking every 2 weeks: weight assessed, progress photos taken',
     },
   ]
 
   const renderCard = (card, index) => {
     return (
-      <Card style={{ width: '18rem' }} key={index} className="Box">
-        <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+      // <Card style={{ width: '18rem' }} key={index} className="Box">
+      //   <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+      //   <Card.Body>
+      //     <Card.Title>{card.title}</Card.Title>
+      //     <Card.Text>{card.text}</Card.Text>
+      //     <Button variant="primary">Go somewhere</Button>
+      //   </Card.Body>
+      // </Card>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" kay={index} className="Box"/>
         <Card.Body>
-          <Card.Title>{card.title}</Card.Title>
-          <Card.Text>{card.text}</Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Title>{card.title1}</Card.Title>
+          <Card.Title>{card.title2}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>{card.list1}</ListGroupItem>
+          <ListGroupItem>{card.list2}</ListGroupItem>
+          <ListGroupItem>{card.list2}</ListGroupItem>
+          <ListGroupItem>{card.list3}</ListGroupItem>
+          <ListGroupItem>{card.list4}</ListGroupItem>
+          <ListGroupItem>{card.list5}</ListGroupItem>
+          <ListGroupItem>{card.list6}</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
         </Card.Body>
       </Card>
     )
   }
 
-  return <div className="grid">
-    {cardInfo.map(renderCard)}
-
-  </div>
+  return <div className="grid">{cardInfo.map(renderCard)}</div>
 }
 
 //  extends Component {
