66d8e79576bde84e955a5f1e92afe0e80269961d
Johnathan <jonathanumana86@gmail.com>
2021-02-03 16:22:57 -0500

    Making the Navbar better, trying styled components

--- fitness/src/App.jsx
+++ fitness/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './components/Home'
-import Header from './components/Header'
+// import Header from './components/Header'
 import Footer from './components/Footer'
 import Plans from './pages/Plans'
 import { Route, Switch} from 'react-router-dom'
@@ -8,7 +8,7 @@ import { Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="app" >
-      <Header />
+      {/* <Header /> */}
       
       <Switch>
         <Route exact path='/' component={Home} />
--- fitness/src/components/Header.jsx
+++ fitness/src/components/Header.jsx
@@ -2,48 +2,59 @@ import React from 'react'
 import './Compon.css'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
-  return (
-    <header className="header">
+
+const NavBarContainer = styled.div`
+  width: 100%;
+  height: 60px;
+  box-shadow: 0 1px 3px  #0f0f0f;
+  opacity: 0.1;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5em;
+`
+
+// const Header = () => {
+//   return (
+//     <header className="header">
 
 
 
 
-      {/* <button className="nav-toggle" aria-label="toggle navigation">
-          <span className="hamburger"></span>
-        </button> */}
+//       {/* <button className="nav-toggle" aria-label="toggle navigation">
+//           <span className="hamburger"></span>
+//         </button> */}
 
-      <nav className="nav">
-        <div className="hamburger">
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
+//       <nav className="nav">
+//         <div className="hamburger">
+//           <div className="line"></div>
+//           <div className="line"></div>
+//           <div className="line"></div>
+//         </div>
 
-        <Link to='/' className="Home-Link">
-          <h1>PRODIGY FITNESS </h1>
-        </Link>
+//         <Link to='/' className="Home-Link">
+//           <h1>PRODIGY FITNESS </h1>
+//         </Link>
 
-        <ul className="nav__list">
+//         <ul className="nav__list">
 
-          <Link to='/'>
-            <li className="nav__item">About Me</li>
-          </Link>
+//           <Link to='/'>
+//             <li className="nav__item">About Me</li>
+//           </Link>
 
-          <Link to='/Plans'>
-            <li className="nav__item">Plans</li>
-          </Link>
+//           <Link to='/Plans'>
+//             <li className="nav__item">Plans</li>
+//           </Link>
 
           
-            <li className="nav__item"><a href="https://docs.google.com/forms/d/e/1FAIpQLSe_GMrl59g4LeierD0XkO2S5yLU6P8b4ItKtrvmfRnj4p4SmA/viewform?usp=sf_link" target="_blank" className="nav__link">Schedule With Me</a></li>
+//             <li className="nav__item"><a href="https://docs.google.com/forms/d/e/1FAIpQLSe_GMrl59g4LeierD0XkO2S5yLU6P8b4ItKtrvmfRnj4p4SmA/viewform?usp=sf_link" target="_blank" className="nav__link">Schedule With Me</a></li>
           
           
           
-        </ul>
-      </nav>
+//         </ul>
+//       </nav>
 
-    </header>
-  )
-}
+//     </header>
+//   )
+// }
 
-export default Header
+// export default Header
