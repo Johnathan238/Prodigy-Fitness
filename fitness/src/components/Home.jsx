1604f9d38a61f11b3a4687fba0b2c7425eeef2e8
Johnathan <jonathanumana86@gmail.com>
2021-02-17 00:50:12 -0500

    I imported ReactPlayer to show users my client progress

--- fitness/src/components/Home.jsx
+++ fitness/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Component } from 'react'
+import ReactPlayer from 'react-player'
 import './Compon.css'
 
 
@@ -17,7 +18,13 @@ export default class Home extends Component {
         <span>Marck is a passionate and ambitious trainer in the fitness industry. His passion for fitness started a year after he began working out. He initially started working out because he was tired of being skinny and decided to make a change to his life. Fast forward to a year later and the changes he saw were tremendous, and that inspired him to get into the industry with hopes of helping as many people as he can do the same thing he did. Marck’s main goal is to help as many people as he can accomplish their desired body goals or reach a point where they are comfortable with how they look AND feel. Fitness to Marck is more than just physical or how you look but more so of how you feel mentally because mental health is extremely important in life especially in today’s age. Marck is currently pursuing his BA in Psychology which will benefit him a lot by giving him the resources of being able to understand people a lot better, by understanding how people’s mind works, Marck will be able to motivate YOU more efficiently.
         Remember, train smart, not hard, <b>Quality-Over-Quantity</b></span>
 
+        <div className="Video">
+          <ReactPlayer url="https://youtu.be/_caukWh354s"/>
+        </div>
+
       </div>
+
+      
     )
   }
 }
