212d662cfce5cc492f4ccc36fe0fa69f6efae243
Johnathan <jonathanumana86@gmail.com>
2021-02-24 15:30:34 -0500

    Making the Navbar way better

--- fitness/src/components/Navbar.jsx
+++ fitness/src/components/Navbar.jsx
@@ -6,51 +6,51 @@ import { NavLinks } from './NavLinks'
 import { DeviceSize } from './responsive'
 import { MobileNavLinks } from './MobileNavLinks'
 
-const NavbarContainer = styled.div`
-  width: 100%;
-  height: 90px;
-  box-shadow: 0 1px 3px rgba(15, 15, 15, 0.13);
-  display: flex;
-  align-items: center;
-  padding: 0 1.5em;
-  background-color: #5FA8D3;
-  text-decoration: none;
-`;
+// const NavbarContainer = styled.div`
+//   width: 100%;
+//   height: 90px;
+//   box-shadow: 0 1px 3px rgba(15, 15, 15, 0.13);
+//   display: flex;
+//   align-items: center;
+//   padding: 0 1.5em;
+//   background-color: #5FA8D3;
+//   text-decoration: none;
+// `;
 
-const LeftSection = styled.div`
-  display: flex;
-  text-decoration: none;
-`;
+// const LeftSection = styled.div`
+//   display: flex;
+//   text-decoration: none;
+// `;
 
-const MiddleSection = styled.div`
-  display: flex;
-  flex: 2;
-  height: 100%;
-  justify-content: flex-end;
-  text-decoration: none;
-`;
+// const MiddleSection = styled.div`
+//   display: flex;
+//   flex: 2;
+//   height: 100%;
+//   justify-content: flex-end;
+//   text-decoration: none;
+// `;
 
-const RightSection = styled.div`
-  display: flex;
-  text-decoration: none;
-`;
+// const RightSection = styled.div`
+//   display: flex;
+//   text-decoration: none;
+// `;
 
-export function Navbar(props) {
-  const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
+// export function Navbar(props) {
+//   const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
   
-  return (
-    <NavbarContainer>
-      <LeftSection>
-        <Logo />
-      </LeftSection>
-      <MiddleSection>{!isMobile && <NavLinks />}</MiddleSection>
-      <RightSection>
-        {!isMobile }
-        {isMobile && <MobileNavLinks />}
-      </RightSection>
-    </NavbarContainer>
-  );
-}
+//   return (
+//     <NavbarContainer>
+//       <LeftSection>
+//         <Logo />
+//       </LeftSection>
+//       <MiddleSection>{!isMobile && <NavLinks />}</MiddleSection>
+//       <RightSection>
+//         {!isMobile }
+//         {isMobile && <MobileNavLinks />}
+//       </RightSection>
+//     </NavbarContainer>
+//   );
+// }
 
 
-export default Navbar
+// export default Navbar
