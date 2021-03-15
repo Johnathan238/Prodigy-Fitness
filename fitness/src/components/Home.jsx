ef0202c9377a8ec159783222f52088b494d8aa35
Johnathan <jonathanumana86@gmail.com>
2021-03-15 10:46:01 -0400

    Got the Table on the HomaPage, just need some touch up

--- fitness/src/components/Home.jsx
+++ fitness/src/components/Home.jsx
@@ -1,47 +1,20 @@
 import React from 'react'
 import { Component } from 'react'
-import { ListGroup, Button, ListGroupItem } from 'react-bootstrap'
+import TablePlan from './TablePlan'
 import Iframe from 'react-iframe'
 import './Compon.css'
-
-const PlanList = () => {
-  const List =[
-      {
-      title: 'HOME',
-      text: 
-        '1 on 1 Training: For those that want the full experience and want to maximize and meet their full physique potential. *Brooklyn based area ONLY* ',
-      list1:
-        'Online Coaching: For those with very busy schedules but are still dedicated to their body goals and want to perform a highly effective exercise in the comfort of their own home, backyard, etc.',
-      list2:
-        'Personalized Workout Plans: For those that are experienced and need a set of workouts based on THEIR OWN body type to follow on their own time. ',
-    }
-  ]
-}
-
-const renderList = (ListGroup, index) => {
-  return (
-  <div class="list-group">
-    <button type="button" class="list-group-item list-group-item-action active">
-    {ListGroupItem.title}
-    </button>
-    <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.text}</button>
-    <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list1}</button>
-    <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list2}</button>
-</div>
-
-  )
-}
-
 
 
 class Home extends Component {
   render() {
     return (
       <div className="Home-Wrapper">
-       
-       {/* <div className="grid">
+      
+      {/* <div className="grid">
           {PlanList.map(renderList)}
         </div> */}
+        <TablePlan />
+
         <img className="Before--After" src="https://i.ibb.co/5vFZZbF/EBD0-B9-EC-2-D97-4-D88-BD4-D-694-DC5-CDFBC5.jpg"
           alt="Before--After"
           height={500}
--- fitness/src/components/TablePlan.jsx
+++ fitness/src/components/TablePlan.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
-import { Component } from 'react'
-import { ListGroup, Button, ListGroupItem } from 'react-bootstrap'
+import {  ListGroupItem } from 'react-bootstrap'
 
 const PlanList = () => {
     const List =[
-        {
+    {
         title: 'HOME',
         text: 
           '1 on 1 Training: For those that want the full experience and want to maximize and meet their full physique potential. *Brooklyn based area ONLY* ',
         list1:
-          'Online Coaching: For those with very busy schedules but are still dedicated to their body goals and want to perform a highly effective exercise in the comfort of their own home, backyard, etc.',
+            'Online Coaching: For those with very busy schedules but are still dedicated to their body goals and want to perform a highly effective exercise in the comfort of their own home, backyard, etc.',
         list2:
-          'Personalized Workout Plans: For those that are experienced and need a set of workouts based on THEIR OWN body type to follow on their own time. ',
-      }
+            'Personalized Workout Plans: For those that are experienced and need a set of workouts based on THEIR OWN body type to follow on their own time. ',
+        }
     ]
-  }
+  
   
   const renderList = (ListGroup, index) => {
     return (
     <div class="list-group">
-      <button type="button" class="list-group-item list-group-item-action active">
-      {ListGroupItem.title}
-      </button>
-      <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.text}</button>
-      <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list1}</button>
-      <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list2}</button>
-  </div>
+        <button type="button" class="list-group-item list-group-item-action active">
+        {ListGroupItem.title}
+        </button>
+        <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.text}</button>
+        <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list1}</button>
+        <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list2}</button>
+    </div>
   
+        )
+    }
+
+    return (
+    <div className="grid"> <h1>Are You Ready to Become A New You  !</h1>
+        {List.map(renderList)} 
+    </div>
     )
-  }
+}
+
+export default PlanList
