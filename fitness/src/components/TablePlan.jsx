1e61fd20bc27f17d4c1be37d2c7edc6a0d2667a7
Johnathan <jonathanumana86@gmail.com>
2021-03-15 10:03:57 -0400

    Made the different plan section for the table

---
+++ fitness/src/components/TablePlan.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Component } from 'react'
+import { ListGroup, Button, ListGroupItem } from 'react-bootstrap'
+
+const PlanList = () => {
+    const List =[
+        {
+        title: 'HOME',
+        text: 
+          '1 on 1 Training: For those that want the full experience and want to maximize and meet their full physique potential. *Brooklyn based area ONLY* ',
+        list1:
+          'Online Coaching: For those with very busy schedules but are still dedicated to their body goals and want to perform a highly effective exercise in the comfort of their own home, backyard, etc.',
+        list2:
+          'Personalized Workout Plans: For those that are experienced and need a set of workouts based on THEIR OWN body type to follow on their own time. ',
+      }
+    ]
+  }
+  
+  const renderList = (ListGroup, index) => {
+    return (
+    <div class="list-group">
+      <button type="button" class="list-group-item list-group-item-action active">
+      {ListGroupItem.title}
+      </button>
+      <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.text}</button>
+      <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list1}</button>
+      <button type="button" class="list-group-item list-group-item-action">{ListGroupItem.list2}</button>
+  </div>
+  
+    )
+  }
